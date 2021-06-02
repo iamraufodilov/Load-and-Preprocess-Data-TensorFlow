2065e3c3c65f3bf2cba887c2f38f022184ccf0a0
raufodilov <applyjobrauf@gmail.com>
2021-06-02 20:39:18 +0800

    updated

--- Text/text_translation.py
+++ Text/text_translation.py
@@ -84,4 +84,123 @@ print("Vocab size: ", vocab_size)
 print("First five vocab entries:", vocab[:5])
 
 
-#
+# create vocabulary table
+keys = vocab
+values = range(2, len(vocab) + 2)  # reserve 0 for padding, 1 for OOV
+
+init = tf.lookup.KeyValueTensorInitializer(
+    keys, values, key_dtype=tf.string, value_dtype=tf.int64)
+
+num_oov_buckets = 1
+vocab_table = tf.lookup.StaticVocabularyTable(init, num_oov_buckets)
+
+
+#Finally, define a fuction to standardize, tokenize and vectorize the dataset
+def preprocess_text(text, label):
+  standardized = tf_text.case_fold_utf8(text)
+  tokenized = tokenizer.tokenize(standardized)
+  vectorized = vocab_table.lookup(tokenized)
+  return vectorized, label
+
+
+#lets look at one sentence and its vectoziation result
+example_text, example_label = next(iter(all_labeled_data))
+print("Sentence: ", example_text.numpy())
+vectorized_text, example_label = preprocess_text(example_text, example_label)
+print("Vectorized sentence: ", vectorized_text.numpy())
+
+
+# now use above function for all dataset with help of map
+all_encoded_data = all_labeled_data.map(preprocess_text)
+
+
+#Split the dataset into train and test
+
+train_data = all_encoded_data.skip(VALIDATION_SIZE).shuffle(BUFFER_SIZE)
+validation_data = all_encoded_data.take(VALIDATION_SIZE)
+
+train_data = train_data.padded_batch(BATCH_SIZE)
+validation_data = validation_data.padded_batch(BATCH_SIZE)
+
+
+#lets look splitted data
+sample_text, sample_labels = next(iter(validation_data))
+print("Text batch shape: ", sample_text.shape)
+print("Label batch shape: ", sample_labels.shape)
+print("First text example: ", sample_text[0])
+print("First label example: ", sample_labels[0])
+
+
+#configure the data
+train_data = configure_dataset(train_data)
+validation_data = configure_dataset(validation_data)
+
+
+#train the model
+model = create_model(vocab_size=vocab_size, num_labels=3)
+model.compile(
+    optimizer='adam',
+    loss=losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+history = model.fit(train_data, validation_data=validation_data, epochs=3)
+
+
+# evaluate the model
+loss, accuracy = model.evaluate(validation_data)
+
+print("Loss: ", loss)
+print("Accuracy: {:2.2%}".format(accuracy))
+
+
+#Export the model
+
+# take all preprocessing functions to one layer
+preprocess_layer = TextVectorization(
+    max_tokens=vocab_size,
+    standardize=tf_text.case_fold_utf8,
+    split=tokenizer.tokenize,
+    output_mode='int',
+    output_sequence_length=MAX_SEQUENCE_LENGTH)
+
+preprocess_layer.set_vocabulary(vocab)
+
+
+#create export the model
+export_model = tf.keras.Sequential(
+    [preprocess_layer, model,
+     layers.Activation('sigmoid')])
+
+export_model.compile(
+    loss=losses.SparseCategoricalCrossentropy(from_logits=False),
+    optimizer='adam',
+    metrics=['accuracy'])
+
+
+# Create a test dataset of raw strings
+test_ds = all_labeled_data.take(VALIDATION_SIZE).batch(BATCH_SIZE)
+test_ds = configure_dataset(test_ds)
+loss, accuracy = export_model.evaluate(test_ds)
+print("Loss: ", loss)
+print("Accuracy: {:2.2%}".format(accuracy))
+
+
+# make prediction on unseen custom data example
+inputs = [
+    "Join'd to th' Ionians with their flowing robes,",  # Label: 1
+    "the allies, and his armour flashed about him so that he seemed to all",  # Label: 2
+    "And with loud clangor of his arms he fell.",  # Label: 0
+]
+predicted_scores = export_model.predict(inputs)
+predicted_labels = tf.argmax(predicted_scores, axis=1)
+for input, label in zip(inputs, predicted_labels):
+  print("Question: ", input)
+  print("Predicted label: ", label.numpy()) # here we goo our model predicted correctly theyir respective label
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# CONCLUSION
+'''
+this model for text classifiction through their labels
+first we load dataset then prepare it to training
+after training we export data in case we put preprocessing into one layer and into model
+'''
